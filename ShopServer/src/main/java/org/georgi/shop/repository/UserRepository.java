e2c4c0006836aaa574ed158a4d292ac72026d2aa
GeorgiNzm <georginzm@gmail.com>
2018-08-29 21:30:51 +0300

    Modify some files

--- ShopServer/src/main/java/org/georgi/shop/repository/UserRepository.java
+++ ShopServer/src/main/java/org/georgi/shop/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByFirstNameAndLastName(String firstName, String lastName);
     User findByEmail(String email);
+    User findByUsername(String username);
 }
