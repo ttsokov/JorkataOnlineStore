070c8186c045382a08f5b160a632a8aa9a76a9bb
GeorgiNzm <georginzm@gmail.com>
2018-08-29 22:13:31 +0300

    Add JWT authentication filter to create the token

---
+++ ShopServer/src/main/java/org/georgi/shop/security/JWTAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package org.georgi.shop.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.georgi.shop.model.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Objects;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+
+        User credentials = null;
+        try {
+            credentials = new ObjectMapper().readValue(request.getInputStream(), User.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return this.authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        Objects.requireNonNull(credentials).getUsername(),
+                        credentials.getPassword(),
+                        new ArrayList<>()
+                )
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String token = JWT.create()
+                        .withSubject(((User) authResult.getPrincipal()).getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + TokenConstants.EXPIRATION_TIME))
+                        .sign(Algorithm.HMAC512(TokenConstants.SECRET));
+
+        response.addHeader(TokenConstants.AUTHORIZATION_HEADER, TokenConstants.BEARER.concat(token));
+    }
+}
