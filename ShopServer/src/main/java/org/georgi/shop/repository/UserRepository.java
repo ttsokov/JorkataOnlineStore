41cc2aa4d436474a6f7f899f74a78bd052f4e118
GeorgiNzm <georginzm@gmail.com>
2018-08-30 14:01:08 +0300

    Anotate UserRepository class, to be component of Spring application

--- ShopServer/src/main/java/org/georgi/shop/repository/UserRepository.java
+++ ShopServer/src/main/java/org/georgi/shop/repository/UserRepository.java
@@ -2,7 +2,10 @@ package org.georgi.shop.repository;
 
 import org.georgi.shop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByFirstNameAndLastName(String firstName, String lastName);
     User findByEmail(String email);
